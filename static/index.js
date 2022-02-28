4be5a518a24804e2c0113f899cb59c257bb38c8b
oktoala <okta.yoga.syah@gmail.com>
2022-02-28 12:46:27 +0800

    Change location of install

--- static/index.js
+++ static/index.js
@@ -18,7 +18,6 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  console.log("Hahah");
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
@@ -27,14 +26,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
   addBtn.style.display = 'block';
 
   addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
       } else {
         console.log('User dismissed the A2HS prompt');
       }
